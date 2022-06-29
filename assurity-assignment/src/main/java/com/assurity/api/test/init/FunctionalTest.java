a6ba803b1262f0b2f805788843b233cde58cf584
Diwakar.B <diwakar.david@gmail.com>
2022-06-30 02:56:15 +0530

    Cleanup

--- assurity-assignment/src/main/java/com/assurity/api/test/init/FunctionalTest.java
+++ assurity-assignment/src/main/java/com/assurity/api/test/init/FunctionalTest.java
@@ -15,36 +15,45 @@ import com.assurity.api.response.pojo.Root;
 import com.assurity.api.util.RestServiceInvoker;
 import com.jayway.jsonpath.JsonPath;
 
+/**
+ * 
+ * @author Diwakar.B
+ * @version 1.0 
+ */
 public class FunctionalTest {
 
+	private static final String JSON_PATH_EXP = "$.Promotions.[?(@.Name=='Gallery')]";
+	public static final String GET_API_URI = "https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?catalogue=false";
+	
+	// Use case to simply validate if the service is available
     @Test
     public void getCategoryDetails_200ResponceReceived_BasicSuccessTest()
             throws ClientProtocolException, IOException {
-    	
-       String endpoint = "https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?catalogue=false";
+       String endpoint = GET_API_URI;
        HttpResponse response= RestServiceInvoker.get(endpoint); 
        Assert.assertEquals(
     		   response.getStatusLine().getStatusCode(),
                 HttpStatus.SC_OK);
     }
     
+    // The core assignment usecase 
     @Test
-    public void getCategoryDetails_validateElementValues_BasicSuccessTest()
+    public void getCategoryDetails_validateElementValues_SuccessTest()
             throws ClientProtocolException, IOException {
-    	
-       String endpoint = "https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?catalogue=false";
+       String endpoint = GET_API_URI;
        Root response= RestServiceInvoker.get(endpoint, Root.class); 
-       //Approach 1 is to validate response by converting into an object
-       Assert.assertEquals(response.getName(), "Carbon credits");
-       Assert.assertEquals(response.isCanRelist(), true);
        
-       //Approach 2 is to validate response by using the jsonpath
+       //Approach 1 is to validate response by converting into an object
+       Assert.assertEquals(response.getName(), "Carbon credits");						// Assertion 1
+       Assert.assertEquals(response.isCanRelist(), true);								// Assertion 2
+       
+       //Approach 2 is to validate response by using the jsonpath using jayway
        HttpResponse httpResponse= RestServiceInvoker.get(endpoint); 
        String responsePayload = EntityUtils.toString(httpResponse.getEntity());
-       String jsonPathExpression = "$.Promotions.[?(@.Name=='Gallery')]";
+       String jsonPathExpression = JSON_PATH_EXP;
        ArrayList jsonValue = JsonPath.parse(responsePayload).read(jsonPathExpression, ArrayList.class);
        LinkedHashMap promotion = (LinkedHashMap) jsonValue.get(0);
        String descr = (String)promotion.get("Description");
-       Assert.assertTrue(descr.contains("Good position in category"));
+       Assert.assertTrue(descr.contains("Good position in category"));					// Assertion 3
     }
 }
